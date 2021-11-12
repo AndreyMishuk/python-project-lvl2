2b89b359008c05d2ac4373201cc8a6a3f5629042
AndreyMishuk <andreygim.am@gmail.com>
2021-11-12 20:01:47 +0300

    add argument -format

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -3,10 +3,12 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(description = 'Generate diff',
-                                    prog = 'gendiff')
+                                     prog = 'gendiff')
+                                     epilog="set format of output")
 
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    parser.add_argument('-f', '--format', default='json')
 
     args = parser.parse_args()
     print(args.accumulate(args.integers))
