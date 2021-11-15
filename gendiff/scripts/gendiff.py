7db5e5047c1398698901977f7e13694fb43cb754
AndreyMishuk <andreygim.am@gmail.com>
2021-11-16 00:31:43 +0300

    create fucntion generated_diff & fucntion gerate_part_dict_diff
    need to add try(open file)

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import json
 import argparse
 
 
@@ -14,7 +15,58 @@ def main():
                         help="set format of output")
 
     args = parser.parse_args()
-    print(args.accumulate(args.integers))
+    print(generated_diff(args.first_file, args.second_file))
+
+
+def generated_diff(first_file, second_file):
+
+    dict_result = {}
+
+    with open(first_file) as file1, open(second_file) as file2:
+
+        dict_file1 = json.load(file1)
+        dict_file2 = json.load(file2)
+        file1_keys = list(dict_file1.keys())
+        file2_keys = list(dict_file2.keys())
+
+        key1_set = frozenset(file1_keys)
+        diff_keys = [item for item in file2_keys if item not in key1_set]
+
+        dict_result.update(generate_part_dict_diff(
+            file1_keys,
+            dict_file1,
+            dict_file2,
+            '- '
+        ))
+
+        dict_result.update(generate_part_dict_diff(
+            diff_keys,
+            dict_file2,
+            dict_file1,
+            '+ '
+        ))
+
+    print(len(dict_result))
+    return dict_result
+
+
+def generate_part_dict_diff(key_dict_1, dict_1, dict_2, sign):
+
+    dict_part_result = {}
+
+    for key in key_dict_1:
+
+        if key in dict_2:
+            if dict_1[key] == dict_2[key]:
+                dict_part_result[key] = dict_2[key]
+                continue
+            else:
+                dict_part_result['- '+str(key)] = dict_1[key]
+                dict_part_result['+ '+str(key)] = dict_2[key]
+
+        dict_part_result[sign+str(key)] = dict_1[key]
+
+    return dict_part_result
 
 
 if __name__ == '__main__':
