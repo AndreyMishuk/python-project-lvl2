d5317d1157c4a863ccb28ee3d2d9a847e0c59020
AndreyMishuk <andreygim.am@gmail.com>
2021-11-17 00:14:02 +0300

    replac functions with list generators
    sorted not work!

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -26,47 +26,44 @@ def generated_diff(first_file, second_file):
 
         dict_file1 = json.load(file1)
         dict_file2 = json.load(file2)
-        file1_keys = list(dict_file1.keys())
-        file2_keys = list(dict_file2.keys())
 
-        key1_set = frozenset(file1_keys)
-        diff_keys = [item for item in file2_keys if item not in key1_set]
+        key_minus = sorted(
+            [key
+                for key in dict_file1
+                    if key not in dict_file2
+            ]
+        )
 
-        dict_result.update(generate_part_dict_diff(
-            file1_keys,
-            dict_file1,
-            dict_file2,
-            '- '
-        ))
+        key_0 = sorted(
+            [key
+                for key in dict_file1
+                    if key in dict_file2
+                    and dict_file1[key] == dict_file2[key]
+            ]
+        )
 
-        dict_result.update(generate_part_dict_diff(
-            diff_keys,
-            dict_file2,
-            dict_file1,
-            '+ '
-        ))
+        key_plus = sorted(
+            [key
+                for key in dict_file2
+                    if key not in dict_file1
+            ]
+        )
 
-    print(len(dict_result))
-    return dict_result
+        key_minus_plus = sorted(
+            [key
+                for key in dict_file1
+                    if key in dict_file2
+                    and dict_file1[key] != dict_file2[key]
+            ]
+        )
 
+    dict_result.update({'- '+str(key): dict_file1[key] for key in key_minus})
+    dict_result.update({'  '+str(key): dict_file1[key] for key in key_0})
+    dict_result.update({'- '+str(key): dict_file1[key] for key in key_minus_plus})
+    dict_result.update({'+ '+str(key): dict_file2[key] for key in key_minus_plus})
+    dict_result.update({'+ '+str(key): dict_file2[key] for key in key_plus})
 
-def generate_part_dict_diff(key_dict_1, dict_1, dict_2, sign):
-
-    dict_part_result = {}
-
-    for key in key_dict_1:
-
-        if key in dict_2:
-            if dict_1[key] == dict_2[key]:
-                dict_part_result[key] = dict_2[key]
-                continue
-            else:
-                dict_part_result['- '+str(key)] = dict_1[key]
-                dict_part_result['+ '+str(key)] = dict_2[key]
-
-        dict_part_result[sign+str(key)] = dict_1[key]
-
-    return dict_part_result
+    return json.dumps(dict_result, indent=4)
 
 
 if __name__ == '__main__':
