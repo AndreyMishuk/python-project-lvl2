551979190478047bc8b85b6e8961a2cc02942cad
AndreyMishuk <andreygim.am@gmail.com>
2022-01-09 18:10:04 +0300

    add strip for content

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -3,7 +3,8 @@ from gendiff.scripts.gendiff import generated_diff
 
 def test_generated_diff():
 
+    diff_str = generated_diff('./tests/fixtures/file1.json',
+                              './tests/fixtures/file2.json')
     with open("./tests/fixtures/result_json.txt", "r") as file_result:
         content = file_result.read()
-        assert generated_diff('./tests/fixtures/file1.json',
-                              './tests/fixtures/file2.json') == content
+        assert diff_str == content.strip()
